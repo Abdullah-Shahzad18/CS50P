9bd402bb9b5f0b4853d5bc9179e555f8c1973392
Abdullah Shahzad <abdullahshahzadnjr@gmail.com>
2025-01-01 21:59:26 +0500

    Update deep.py

--- Week 1 Problem Set/deep.py
+++ Week 1 Problem Set/deep.py
@@ -1,6 +1,5 @@
-answer = input("What is the Answer of the Great Question of Life, The Universe, and Everything?")
-
-if answer=="42" or answer=="forty-two" or answer=="forty two" or answer=="forty-two".capitalize() or answer=="forty two".capitalize() or answer=="forty-two".lower() or answer=="forty-two".upper() or answer=="forty two".lower() or answer=="forty two".upper():
+answer = input("What is the Answer of the Great Question of Life, The Universe, and Everything?").lower().strip()
+if answer=="42".strip("") or answer=="forty-two" or answer=="forty two" or answer=="forty-two".capitalize() or answer=="forty two".capitalize() or answer=="forty-two".lower() or answer=="forty-two".upper() or answer=="forty two".lower() or answer=="forty two".upper():
     print("Yes")
 else:
-    print("No")     
+    print("No")
