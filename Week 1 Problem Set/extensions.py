cca1416f52816fff81c28d73f6b00ecc565c6e60
Abdullah Shahzad <abdullahshahzadnjr@gmail.com>
2025-01-01 22:00:00 +0500

    Update extensions.py

--- Week 1 Problem Set/extensions.py
+++ Week 1 Problem Set/extensions.py
@@ -1,22 +1,21 @@
+filename = input("Enter the filename").lower().strip()
 
-filename = input()
 
-def filetype(extension):
-    if extension.endswith(".gif"):
-        print("image/gif")
-    elif extension.endswith(".jpg") or (".jpeg"):
-        print("image/jpeg")
-    elif extension.endswith(".png"):
-        print("image/png")
-    elif extension.endswith(".pdf"):
-        print("application/pdf")
-    elif extension.endswith(".txt"):
-        print("text/plain")
-    elif extension.endswith(".zip"):
-        print("application/zip")
-    else:
-        print("application/octet-stream")
-
-filetype(filename)
+if filename.endswith(".gif")==True:
+    print("image/gif")
+elif filename.endswith(".jpg")==True:
+    print("image/jpeg")
+elif filename.endswith(".jpeg")==True:
+    print("image/jpeg")
+elif filename.endswith(".png")==True:
+    print("image/png")
+elif filename.endswith(".pdf")==True:
+    print("application/pdf")
+elif filename.endswith(".txt")==True:
+    print("text/plain")
+elif filename.endswith(".zip")==True:
+    print("application/zip")
+else:
+    print("application/octet-stream")
 
 
