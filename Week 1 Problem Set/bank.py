654a2a1f34695276416b0d365fa85e12309561e8
Abdullah Shahzad <abdullahshahzadnjr@gmail.com>
2025-01-01 21:57:35 +0500

    Update bank.py

--- Week 1 Problem Set/bank.py
+++ Week 1 Problem Set/bank.py
@@ -1,12 +1,9 @@
-greet = input("Greetings: ").lower()
-
-
-if greet.startswith("hello")==True:
-    print("0$")
-elif greet.startswith("h")==True and greet!="hello":
-    print("20$")
+answer = input("What is the Answer of the Great Question of Life, The Universe, and Everything?").lower().strip()
+if answer=="42".strip("") or answer=="forty-two" or answer=="forty two" or answer=="forty-two".capitalize() or answer=="forty two".capitalize() or answer=="forty-two".lower() or answer=="forty-two".upper() or answer=="forty two".lower() or answer=="forty two".upper():
+    print("Yes")
 else:
-    print("100$")
+    print("No")
+
 
      
 
